791b9acc2d7fdd0f1875a76c78c091805b4c56da
Ho Nguyen Thanh Tam <hnttam.sgt@gmail.com>
2020-04-06 12:08:25 +0700

    Frist init

--- src/main.ts
+++ src/main.ts
@@ -1,26 +1,5 @@
 'use strict';
+import {Log} from './log'
 
-export const foo = 'bar';
-
-export const r2gSmokeTest = function () {
-  // r2g command line app uses this exported function
-  return true;
-};
-
-/*
-
- TODO: bad library design:
- 
- module.exports = 'foo';
- export = 'foo';
-
- TODO: good library design:
- 
- exports.x = 'foo'
- export const x = 'foo'
- export default = 'foo';
-
-*/
-
-
+export const Logger = new Log();
 
