2b1e4da8f730953837c94cc998c405c7aac7e53f
Ho Nguyen Thanh Tam <hnttam.sgt@gmail.com>
2020-04-07 00:48:54 +0700

    Adding nullable for detail

--- lib/logger.d.ts
+++ lib/logger.d.ts
@@ -5,10 +5,10 @@ export declare class Logger implements LogInterface {
   private logLevel;
   private allLevels;
   constructor(logLevelStr?: string);
-  debug(msg: string, supportingDetails: any[]): void;
-  info(msg: string, supportingDetails: any[]): void;
-  warn(msg: string, supportingDetails: any[]): void;
-  error(msg: string, supportingDetails: any[]): void;
+  debug(msg: string, supportingDetails?: any[]): void;
+  info(msg: string, supportingDetails?: any[]): void;
+  warn(msg: string, supportingDetails?: any[]): void;
+  error(msg: string, supportingDetails?: any[]): void;
   private emitLogMessage;
   private createStack;
 }
--- src/logger.ts
+++ src/logger.ts
@@ -12,20 +12,20 @@ export class Logger implements LogInterface {
     this.logLevel = new LogLevel(this.level.toNumber(), this.level.toString());
   }
 
-  public debug(msg: string, supportingDetails: any[]): void {
+  public debug(msg: string, supportingDetails?: any[]): void {
     this.emitLogMessage(this.allLevels.DEBUG, msg, supportingDetails);
   }
 
-  public info(msg: string, supportingDetails: any[]): void {
+  public info(msg: string, supportingDetails?: any[]): void {
     this.emitLogMessage(this.allLevels.INFO, msg, supportingDetails);
   }
 
-  public warn(msg: string, supportingDetails: any[]): void {
+  public warn(msg: string, supportingDetails?: any[]): void {
     this.emitLogMessage(this.allLevels.WARN, msg, supportingDetails);
   }
 
-  public error(msg: string, supportingDetails: any[]): void {
-    if (supportingDetails.length > 0) {
+  public error(msg: string, supportingDetails?: any[]): void {
+    if (Array.isArray(supportingDetails) && supportingDetails.length > 0) {
       const stack = '\n' + this.createStack() + '\n';
       supportingDetails.push(stack);
     }
@@ -36,12 +36,12 @@ export class Logger implements LogInterface {
   private emitLogMessage(
     msgType: LogLevel,
     msg: string,
-    supportingDetails: any[],
+    supportingDetails?: any[],
   ) {
     if (this.logLevel.isEnableLog(msgType)) {
       const logTime = moment(new Date()).format('YYYY-MM-DD HH:mm:ss');
       console.log(`[${logTime}] | [${msgType.toString()}] | ${msg}`);
-      if (supportingDetails.length > 0) {
+      if (Array.isArray(supportingDetails) && supportingDetails.length > 0) {
         console.log(
           `[${logTime}] | [${msgType.toString()}] | ${supportingDetails}`,
         );
